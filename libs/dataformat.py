101faf845a936d07b1796a6b3c9a50816e230718
Wellyngton Marcondes <wmarcondesbr@gmail.com>
2022-12-05 13:51:00 -0300

    Botões Painel

--- libs/dataformat.py
+++ libs/dataformat.py
@@ -5,7 +5,7 @@ TO_LOGIN = "<li class=\"nav-button\"><a href=\"/login\"  class=\"btn btn-outline
 LOGED = "<li class=\"nav-button\"><a href=\"/logout\" class=\"btn btn-dark btn-block btn-lg\">SAIR</a></li>"
 USER_PAINEL = "<li class=\"nav-button\"><a href=\"/user\">USUÁRIO</a></li>"
 INDEX_ADMIN_PAINEL = "<li class=\"nav-button\"><a href=\"/painel\" class=\"btn btn-outline-dark\">PAINEL</a></li>"
-PAINEL_REGISTER_BUTTON = "<li class=\"nav-button\"><a href=\"/register\" class=\"btn btn-outline-dark btn-block btn-lg\"  data-bs-toggle=\"tooltip\" data-bs-title=\"Cadastro de usuário\">USUÁRIO</a></li>"
+PAINEL_REGISTER_BUTTON = "<li class=\"nav-button\"><a href=\"/register\" class=\"btn btn-outline-dark btn-block\"  data-bs-toggle=\"tooltip\" data-bs-title=\"Cadastro de usuário\">USUÁRIO</a></li>"
 ADMIN_REGISTER_CHECK = "<div style=\"display:flex;\"><input class=\"form-check-input\" style= \"padding: 10px;\" type=\"checkbox\" id=\"worker\" name=\"worker\" value=\"worker\" aria-label=\"...\"/> <div style=\"text-align: center; padding:2px\"> Funcionário</div></div>"
 
 #mensagens de erro
